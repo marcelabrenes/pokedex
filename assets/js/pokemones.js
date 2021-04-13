1945e7ed233f3490f99f2734955998b176a2baa7
Marcela Brenes Daniels <marce89455@gmail.com>
2021-04-13 10:28:46 -0600

    first commit

--- assets/js/pokemones.js
+++ assets/js/pokemones.js
@@ -1,1 +1,23 @@
 console.log('hola mundo')
+$(document).ready(function(){
+  //$.ajax('https://pokeapi.co/api/v2/pokemon/')
+  //  .done(function(data){
+  //     debugger;
+  //    data.results.forEach(function(pokemon){
+  //      addPokemon(pokemon);
+  //    });
+  //    $('#more-pokemons').attr('data-next', data.next);
+  //  }); 
+
+
+  fetch('https://pokeapi.co/api/v2/pokemon/')
+    .then(function(data){
+      return data.json()
+    })
+    .then(function(data){
+      console.log(data)
+      data.results.forEach(pokemon => {
+        document.querySelector('#pokemons').innerHTML = `<h1>${pokemon.name}</h1>`
+      });
+    })
+})
